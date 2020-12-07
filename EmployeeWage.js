e0000e25087dc5e2b379503ed37e8cf06a8eb7d6
Apoorva1217 <apoorvarasal12@gmail.com>
2020-12-07 18:55:59 +0530

    [Apoorva] Refactor : Refactor the Code to write a function to get work hours

--- EmployeeWage.js
+++ EmployeeWage.js
@@ -9,6 +9,7 @@ else {
 }
 
 //UC2 Ability to Calculate Daily Employee Wage based on part time or full time work
+//UC3 Refactor the Code to write a function to get work hours
 const IS_PART_TIME = 1;
 const IS_FULL_TIME = 2;
 const PART_TIME_HOURS = 4;
@@ -16,16 +17,17 @@ const FULL_TIME_HOURS = 8;
 const WAGE_PER_HOUR = 20;
 
 let empHrs = 0;
-let empCheck = Math.floor(Math.random() * 10) % 3;
-switch (empCheck) {
-    case IS_PART_TIME:
-        empHrs = PART_TIME_HOURS;
-        break;
-    case IS_FULL_TIME:
-        empHrs = FULL_TIME_HOURS;
-        break;
-    default:
-        empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+function getWorkingHours(empCheck) {
+    switch (empCheck) {
+        case IS_PART_TIME:
+            return PART_TIME_HOURS;
+        case IS_FULL_TIME:
+            return FULL_TIME_HOURS;
+        default:
+            return 0;
+    }
 }
+empHrs=getWorkingHours(empCheck);
 let empWage = empHrs * WAGE_PER_HOUR;
 console.log("Employee Wage is : " + empWage);
